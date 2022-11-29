0c45f3855520e043d8bab70b54adac8e194ebef5
Behilam <praehil@gmail.com>
2022-11-29 15:22:36 -0500

    Add check for invalid users collection
    Throws error if collection doesn't have a field named 'role'

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,4 +1,6 @@
 import type { Access, Config, Plugin } from 'payload/config';
+import type { FieldAffectingData } from 'payload/dist/fields/config/types';
+import { fieldAffectsData } from 'payload/dist/fields/config/types';
 
 const executiveRoles = ['admin', 'executive'];
 const isExecutive: Access<{ role: string }> = ({ data }) =>
@@ -7,6 +9,19 @@ const isExecutive: Access<{ role: string }> = ({ data }) =>
 export const defaultExecutiveAccess =
   (ignoredSlugs: string[]): Plugin =>
   (incomingConfig: Config): Config => {
+    const userCollectionSlug = incomingConfig.admin?.user ?? 'user';
+    const userCollection = incomingConfig.collections?.find(
+      collection => collection.slug === userCollectionSlug
+    );
+
+    const dataFields = userCollection?.fields.filter(fieldAffectsData) as
+      | FieldAffectingData[]
+      | undefined;
+
+    const hasRoleField = dataFields?.map(field => field.name).includes('role');
+    if (!hasRoleField)
+      throw new Error('Users collection must have a field named `role`');
+
     const config: Config = {
       ...incomingConfig,
       collections: incomingConfig.collections?.map(collection => {
